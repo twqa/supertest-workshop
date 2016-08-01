18f3e20a3cf8c7bf7eacdf27d2832ed09ec8ab19
Pingping Yao <ppyao@thoughtworks.com>
2016-08-01 19:05:54 +0800

    add some tests

--- test/supertest-workshop_books.js
+++ test/supertest-workshop_books.js
@@ -22,9 +22,49 @@ describe('Test Google Books API' ,function(){
 
             request
                 .get('?q=test')
-                .expect(200, done)
+                //.get('/')
+                //.query({
+                //q : "test"
+                //    MaxResult:2
+                // })
+                .expect(200,done)
 
-        })
+        });
+
+        it('Search books with name parameter(q="cucumber") and ) - Return 200 ok',function(done){
+            this.timeout(80000)
+            console.log("check")
+            //request = request(endpoint +'/volumes')
+
+            request.get('/')
+                .query({ q: ' cucumber' ,MaxResult:2})
+                .expect(function (res) {
+                    //console.log(res)
+                    console.log("test")
+                   expect(res.body.items[0].id).to.equal('0dge3Xh6EjUC')
+                    //done()
+                }).end(done)
+
+        });
+
+        it('Retrieves a Volume resource based on ID that is from last case - Return 200', function(done){
+
+            this.timeout(80000)
+
+            request.get('/' + id)
+
+                .expect(function(res){
+
+                    // Check the id in response is the same as parameter
+                    expect(res.body.id).to.equal(id)
+
+                })
+
+                .end(function(err,res){
+
+                    done(err);
+
+                })
 
 
     })
